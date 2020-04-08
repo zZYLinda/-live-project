7699e0c8415ef9699477c5c188cd8d6b17da2023
pig <60469923+pigpig922@users.noreply.github.com>
2020-04-08 20:37:37 +0800

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,19 +1,35 @@
-import tkinter
+import tkinter as tk
+import tkinter.messagebox as mbox
+ 
 
-# 定义一个窗体
-
-top = tkinter.Tk(className="校记录登记")
-
-# 将其加入主循环
-Label(top,text="学号").grid(row=0)
-Label(top,text="姓名").grid(row=1)
-Label(top,text="进入时间").grid(row=2)
-
-e1=Entry(top)
-e2=Entry(top)
-e3=Entry(top)
-
-e1.grid(row=0,column=1)
-e2.grid(row=1,column=1)
-e3.grid(row=2,column=1)
-top.mainloop()
+class MainUI(tk.Frame):
+# Application构造函数，master为窗口的父控件
+ def __init__(self, master = None):
+# 初始化Application的Frame部分 
+  tk.Frame.__init__(self, master)
+# 显示窗口，并使用grid布局
+  self.grid()
+# 创建控件
+  self.createWidgets()
+ # 创建控件
+ def createWidgets(self):
+# 创建一个标签，输出要显示的内容（更多Python学习资料加群631441315）
+  self.firstLabel = tk.Label(self,text="登记姓名、学号和进入时间")
+# 设定使用grid布局
+  self.firstLabel.grid()
+# 创建一个按钮，用来触发answer方法
+  self.clickButton = tk.Button(self,text="进入",command=self.answer)
+# 设定使用grid布局
+  self.clickButton.grid()
+ def answer(self):
+# 我们通过 messagebox 来显示一个提示框
+   mbox.showinfo("「校园人口调查」",'''学校人员+1''')
+    
+# 创建一个MainUI对象
+app = MainUI()
+# 设置窗口标题
+app.master.title('「校园人口调查」')
+# 设置窗体大小
+app.master.geometry('300x200')
+# 主循环开始
+app.mainloop()
